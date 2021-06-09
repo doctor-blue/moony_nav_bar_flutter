2e46bd5bfe4b24333925b86fa545cfee99a57da0
Nguyen Van Tan <doctorblue.king@gmail.com>
2021-06-09 22:33:23 +0700

    Init

---
+++ ios/Classes/MoonyNavBarPlugin.h
@@ -0,0 +1,4 @@
+#import <Flutter/Flutter.h>
+
+@interface MoonyNavBarPlugin : NSObject<FlutterPlugin>
+@end
---
+++ test/moony_nav_bar_test.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/services.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:moony_nav_bar/moony_nav_bar.dart';
+
+void main() {
+  const MethodChannel channel = MethodChannel('moony_nav_bar');
+
+  TestWidgetsFlutterBinding.ensureInitialized();
+
+  setUp(() {
+    channel.setMockMethodCallHandler((MethodCall methodCall) async {
+      return '42';
+    });
+  });
+
+  tearDown(() {
+    channel.setMockMethodCallHandler(null);
+  });
+
+  test('getPlatformVersion', () async {
+    expect(await MoonyNavBar.platformVersion, '42');
+  });
+}
